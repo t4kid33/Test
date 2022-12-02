005d309fc748c095fff33fbdab689b4918614b31
t4kid33 <upcard007@gmail.com>
2022-12-02 14:35:23 +0700

    first commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="../assets/lao.gif"
+      style="width: 200px; height: 95px"
+    >
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
